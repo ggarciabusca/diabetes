e2c89217abebc8f8221f5216b998f18b80051768
Gonzalo Garcia <ggarciabusca@gmail.com>
2022-11-14 22:44:36 -0300

    primer version de los templates

--- carbohidratos/urls.py
+++ carbohidratos/urls.py
@@ -3,5 +3,9 @@ from django.urls import path
 from carbohidratos.views import *
 
 urlpatterns = [
-    path("inicio/", inicio,name="cabohidratos-inicio"),
+    path("inicio/", inicio,name="carbohidratos-inicio"),
+    path("personas/", personas,name="carbohidratos-personas"),
+    path("comidas/", comidas,name="carbohidratos-comidas"),
+    path("mediciones/", mediciones,name="carbohidratos-mediciones"),
+    path("alimentos/", alimentos,name="carbohidratos-alimentos")
 ]
--- carbohidratos/views.py
+++ carbohidratos/views.py
@@ -5,5 +5,21 @@ from carbohidratos.models import *
 # Create your views here.
 
 def inicio(request):
-    return render(request,"carbohidratos/index.html")
+    return render(request,"carbohidratos/base.html")
+    pass
+
+def personas(request):
+    return render(request,"carbohidratos/personas.html")
+    pass
+
+def comidas(request):
+    return render(request,"carbohidratos/comidas.html")
+    pass
+
+def mediciones(request):
+    return render(request,"carbohidratos/mediciones.html")
+    pass
+
+def alimentos(request):
+    return render(request,"carbohidratos/alimentos.html")
     pass
